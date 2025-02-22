d78b91d1d8fe71123de4a7a21a691b8fa7600862
Borja ThePortableOne <borja.pinero@gmail.com>
2025-02-22 19:16:58 +0100

    Constructor de vehiculo Getter y setter objeto vehiculo

--- src/main/java/Modelos/Vehiculo.java
+++ src/main/java/Modelos/Vehiculo.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package Modelos;
 
 /**
@@ -13,5 +12,92 @@ package Modelos;
  * @author Borja Piñero
  */
 public class Vehiculo {
+
+    private String matricula,
+            marca,
+            descripcion,
+            nombrePropietario,
+            dni;
+    private double precio,
+            km;
+
+    public Vehiculo() {
+        this.matricula = "1234AAA";
+        this.marca = "Hyundai";
+        this.descripcion = "Descripcion por defecto";
+        this.nombrePropietario = "Fulanito Tal Cual";
+        this.dni = "123456789A";
+        this.precio = 10000;
+        this.km = 100000;
+
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombrePropietario() {
+        return nombrePropietario;
+    }
+
+    public void setNombrePropietario(String nombrePropietario) {
+        this.nombrePropietario = nombrePropietario;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getKm() {
+        return km;
+    }
+
+    public void setKm(double km) {
+        this.km = km;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo:"
+                + "\nmatricula=" + matricula 
+                + "\nmarca=" + marca 
+                + "\ndescripcion=" + descripcion 
+                + "\nnombrePropietario=" + nombrePropietario 
+                + "\ndni=" + dni 
+                + "\nprecio=" + precio 
+                + "\nkm=" + km + '}';
+    }
 
 }
