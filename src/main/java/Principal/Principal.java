febbfb644a72cb8b838cf6a01a45a47b9a98f6fc
Borja ThePortableOne <borja.pinero@gmail.com>
2025-02-22 18:33:16 +0100

    Añadidio menu

--- src/main/java/Principal/Principal.java
+++ src/main/java/Principal/Principal.java
@@ -4,6 +4,8 @@
 
 package Principal;
 
+import java.util.Scanner;
+
 /**
  *
  * @author borja
@@ -11,6 +13,86 @@ package Principal;
 public class Principal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        int opcion;
+
+        Scanner entrada = new Scanner(System.in);
+
+        do {
+
+            System.out.println("***********************************");
+            System.out.println("*     Gestor de concesionario     *");
+            System.out.println("***********************************");
+            System.out.println("[1] - Nuevo vehiculo");
+            System.out.println("[2] - Listar vehiculo");
+            System.out.println("[3] - Buscar vehiculo");
+            System.out.println("[4] - Modificar km vehiculo");
+            System.out.println("[5] - Eliminar vehiculo");
+            System.out.println("[6] - Salir             [0] - Ayuda");
+            System.out.println("***********************************");
+
+            opcion = entrada.nextInt();
+
+            switch (opcion) {
+
+                case 1 -> {
+                    System.out.println("***********************************");
+                    System.out.println("*         Nuevo vehiculo          *");
+                    System.out.println("***********************************");
+
+                }
+
+                case 2 -> {
+                    System.out.println("***********************************");
+                    System.out.println("*         Listar vehiculo         *");
+                    System.out.println("***********************************");
+
+                }
+
+                case 3 -> {
+                    System.out.println("***********************************");
+                    System.out.println("*         Buscar vehiculo         *");
+                    System.out.println("***********************************");
+
+                }
+
+                case 4 -> {
+                    System.out.println("***********************************");
+                    System.out.println("*          Modificar km           *");
+                    System.out.println("***********************************");
+
+                }
+
+                case 5 -> {
+                    System.out.println("***********************************");
+                    System.out.println("*        Eliminar vehiculo        *");
+                    System.out.println("***********************************");
+
+                }
+
+                case 6 -> {
+                    System.out.println("Cerrando inventario. Hasta pronto");
+                    
+                }
+                
+                case 0 -> {
+                    System.out.println("***********************************");
+                    System.out.println("*              Ayuda              *");
+                    System.out.println("***********************************");
+                    System.out.println("[1] - Anade un nuevo vehiculo");
+                    System.out.println("[2] - Lista los vehiculos del");
+                    System.out.println("      concesionario");
+                    System.out.println("[3] - Busca un vehiculo por");
+                    System.out.println("      matricula");
+                    System.out.println("[4] - Modifica los km de un");
+                    System.out.println("      vehiculo");
+                    System.out.println("[5] - Elimina un vehiculo del");
+                    System.out.println("      concesionario");
+                                    
+                }
+            }
+
+        } while (opcion != 6);
+
     }
 }
