81c155c59385b509e6ab78900dc3f42ca6ba8cc8
Borja ThePortableOne <borja.pinero@gmail.com>
2025-02-24 20:06:10 +0100

    Añadido método enterParaSalir

---
+++ src/main/java/Utilidades/Utilidades.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utilidades;
+
+import java.util.Scanner;
+
+/**
+ * Programación DAM Modalidad Virtual
+ *
+ * Curso 2024/2025
+ *
+ * @author Borja Piñero
+ */
+public class Utilidades {
+    
+    /**
+     * Pausa la ejecución del programa hasta que el usuario presiones la tecla
+     * ENTER, permitiendo una mejor lectura de los elementos de cada submenú
+     * 
+     */
+    public  static void enterParaSalir() {
+        
+        Scanner entrada = new Scanner (System.in);
+        
+        System.out.println();
+
+        System.out.print("Presiona ENTER para continuar...");
+        System.out.println();
+
+        entrada.nextLine();
+
+    }
+
+}
