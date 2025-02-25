3e964c5f2d8315c0266399d461e2e029a3becca6
Borja ThePortableOne <borja.pinero@gmail.com>
2025-02-25 23:48:23 +0100

    añadido metodo calcularLetraDni

--- src/main/java/com/damel/utilidades/Utilidades.java
+++ src/main/java/com/damel/utilidades/Utilidades.java
@@ -14,22 +14,36 @@ import java.util.Scanner;
  * @author Borja Piñero
  */
 public class Utilidades {
-    
+
     /**
      * Pausa la ejecución del programa hasta que el usuario presiones la tecla
      * ENTER, permitiendo una mejor lectura de los elementos de cada submenú
-     * 
+     *
      */
-    public  static void enterParaSalir() {
-        
-        Scanner entrada = new Scanner (System.in);
-        
+    public static void enterParaSalir() {
+
+        Scanner entrada = new Scanner(System.in);
+
         System.out.println();
 
         System.out.print("Presiona ENTER para continuar...");
         System.out.println();
 
         entrada.nextLine();
+
+    }
+
+    public static String calcularLetraDni(String dni) {
+        String[] letraDni = {"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D",
+            "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"};
+
+        String dniSinLetra = dni.substring(0, 8);
+
+        int numDni = Integer.parseInt(dniSinLetra);
+
+        int posicionTabla = numDni % 23;
+
+        return letraDni[posicionTabla];
 
     }
 
