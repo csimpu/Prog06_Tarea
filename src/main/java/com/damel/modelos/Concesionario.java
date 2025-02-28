3e63a5390e4a25f623d18df1b7fc41a6a5ac0687
Borja TheFirstOne <borja.pinero@gmail.com>
2025-02-28 02:16:26 +0100

    añadidos metodo modificarKm

--- src/main/java/com/damel/modelos/Concesionario.java
+++ src/main/java/com/damel/modelos/Concesionario.java
@@ -26,12 +26,9 @@ public class Concesionario {
 
     public int insertarVehiculo(Vehiculo nuevoVehiculo) {
 
-        
-        if (!Validaciones.matriculaEsUnica(nuevoVehiculo, vehiculosAlmacenados, numVehiculo)){
+        if (!Validaciones.matriculaEsUnica(nuevoVehiculo, vehiculosAlmacenados, numVehiculo)) {
             return -2;
-        }
-
-        else if (numVehiculo < vehiculosAlmacenados.length) {
+        } else if (numVehiculo < vehiculosAlmacenados.length) {
             vehiculosAlmacenados[numVehiculo] = nuevoVehiculo;
             numVehiculo++;
             return 0;
@@ -43,31 +40,43 @@ public class Concesionario {
     }
 
     public String buscaVehiculo(String matricula) {
-        
+
         for (int j = 0; j < numVehiculo; j++) {
-            if (vehiculosAlmacenados[j].getMatricula().equals(matricula)){
+            if (vehiculosAlmacenados[j].getMatricula().equals(matricula)) {
                 return vehiculosAlmacenados[j].toString();
             }
-            
-        } return null;
+
+        }
+        return null;
 
     }
-    
-    public String listaVehiculos(){
-        
+
+    public String listaVehiculos() {
+
         StringBuilder lista = new StringBuilder();
-        
+
         lista.append("Vehiculos en el concesionario:\n");
-                
+
         for (int k = 0; k < numVehiculo; k++) {
             lista.append("Vehiculo ")
-                    .append(k+1)
+                    .append(k + 1)
                     .append("\n")
                     .append(vehiculosAlmacenados[k].toString())
                     .append("\n          ***\n");
         }
-        
+
         return lista.toString();
     }
 
+    public int modificarKm(String matricula, double nuevoKm) {
+        for (int m = 0; m < numVehiculo; m++) {
+            if (vehiculosAlmacenados[m].getMatricula().equals(matricula)) {
+
+                vehiculosAlmacenados[m].setKm(nuevoKm);
+                return 0;
+            }
+
+        }
+        return -1;
+    }
 }
